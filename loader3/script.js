ee12cf75de1b722e80837e0e947c00cf2bf91245
mhr1235 <65504141+mhr1235@users.noreply.github.com>
2022-03-20 19:04:40 -0400

    Add files via upload

---
+++ loader3/script.js
@@ -0,0 +1,193 @@
+const play = `<?xml version="1.0" encoding="utf-8"?>
+<!-- Generator: Adobe Illustrator 24.0.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+   viewBox="0 0 337.2 337.2" style="enable-background:new 0 0 337.2 337.2;" xml:space="preserve">
+<style type="text/css">
+  .st0{display:none;}
+  .st1{display:inline;}
+  .st2{fill:#010002;}
+  .st3{fill:#FFFFFF;stroke:#CCCCCC;stroke-miterlimit:10;}
+</style>
+<g id="Capa_1" class="st0">
+  <g class="st1">
+    <g>
+      <path class="st2" d="M33.9,337.2V0l285.3,168.6L33.9,337.2z M50.2,28.5v280.2l237.1-140.1L50.2,28.5z"/>
+    </g>
+  </g>
+</g>
+<g id="Layer_2">
+  <polygon class="st3" points="50,28.5 50,308.7 287.1,168.6   "/>
+</g>
+</svg>`;
+const pause = /*`<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+ </svg>`;*/
+`
+<!-- Generator: Adobe Illustrator 24.0.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+   viewBox="0 0 337.2 337.2" style="enable-background:new 0 0 337.2 337.2;" xml:space="preserve">
+<style type="text/css">
+  .st0{display:none;}
+  .st1{display:inline;}
+  .st2{fill:#010002;}
+  .st3{display:inline;fill:#FFFFFF;stroke:#CCCCCC;stroke-miterlimit:10;}
+  .st4{fill:#FFFFFF;stroke:#CCCCCC;stroke-miterlimit:10;}
+</style>
+<g id="Layer_3">
+  <rect x="50" y="28.5" class="st4" width="80.8" height="280.2"/>
+  <rect x="206.3" y="28.5" class="st4" width="80.8" height="280.2"/>
+</g>
+</svg>`;
+const sound = /*`<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" />
+</svg>`;*/
+
+`<svg version="1.1"
+   id="svg2" inkscape:export-filename="/home/qubodup/x22.png" inkscape:export-xdpi="90" inkscape:export-ydpi="90" inkscape:version="0.91 r13725" sodipodi:docname="minimal speaker icon.svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 393.5 453.8"
+   style="enable-background:new 0 0 393.5 453.8;" xml:space="preserve">
+<style type="text/css">
+  .st0{fill:#FFFFFF;stroke:#CCCCCC;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;}
+  .st1{fill:#FFFFFF;stroke:#CCCCCC;stroke-width:2;stroke-miterlimit:10;}
+</style>
+<sodipodi:namedview  bordercolor="#666666" borderopacity="1.0" fit-margin-bottom="0" fit-margin-left="0" fit-margin-right="0" fit-margin-top="0" id="base" inkscape:current-layer="layer1" inkscape:cx="214.07975" inkscape:cy="309.42562" inkscape:document-units="px" inkscape:object-nodes="true" inkscape:pageopacity="0.0" inkscape:pageshadow="2" inkscape:snap-bbox="false" inkscape:snap-object-midpoints="true" inkscape:snap-smooth-nodes="true" inkscape:window-height="865" inkscape:window-maximized="0" inkscape:window-width="1734" inkscape:window-x="73" inkscape:window-y="130" inkscape:zoom="0.70710678" pagecolor="#ffffff" showgrid="false" units="px">
+  </sodipodi:namedview>
+<g id="layer1" transform="translate(-55,-285.4683)" inkscape:groupmode="layer" inkscape:label="Layer 1">
+  
+    <path id="path4241" inkscape:connector-curvature="0" sodipodi:nodetypes="ccssccccscccccccsscccscccccccccsssccccccscccc" class="st0" d="
+    M345.7,340.2l0,0.2l-19.5,16.8c0,0,12.9,12.2,26.2,38.1c13.3,25.9,26.7,65.3,26.7,119s-13.5,92-26.7,117s-26,36.1-26,36.1l18.1,17
+    c0,0,21.9-19.8,36.1-46.5c14.2-26.8,28.2-67.6,28.2-123.6c0-56-14.1-97.7-28.2-125.3C366.5,361.4,345.7,340.2,345.7,340.2
+    L345.7,340.2z M246.3,357.1c-2.4-0.2-5.7,1.2-10.4,4.4c-18.8,12.9-87.1,87.3-87.1,87.3c-0.6,0-1.1,0.2-1.6,0.3H94.6V580h54.3
+    c0,0,67.1,74.8,87.1,87.6c20,12.9,15.9-7.1,15.9-15.9V377.1C251.8,370.5,253.4,357.7,246.3,357.1L246.3,357.1z M286.2,387.9
+    l-20.3,19c0,0,47.7,33.5,47.7,106.7c0,73.1-47.4,104.4-47.4,104.4l19.8,21.2c0,0,57.4-46.4,57.4-125.6
+    C343.4,434.4,286.2,387.9,286.2,387.9L286.2,387.9z"/>
+  
+    <path id="path2842" inkscape:connector-curvature="0" sodipodi:nodetypes="ccssccccscccccccsscccscccccccccsssccccccscccc" class="st1" d="
+    M345.7,340.2l0,0.2l-19.5,16.8c0,0,12.9,12.2,26.2,38.1c13.3,25.9,26.7,65.3,26.7,119s-13.5,92-26.7,117s-26,36.1-26,36.1l18.1,17
+    c0,0,21.9-19.8,36.1-46.5c14.2-26.8,28.2-67.6,28.2-123.6c0-56-14.1-97.7-28.2-125.3C366.5,361.4,345.7,340.2,345.7,340.2
+    L345.7,340.2z M246.3,357.1c-2.4-0.2-5.7,1.2-10.4,4.4c-18.8,12.9-87.1,87.3-87.1,87.3c-0.6,0-1.1,0.2-1.6,0.3H94.6V580h54.3
+    c0,0,67.1,74.8,87.1,87.6c20,12.9,15.9-7.1,15.9-15.9V377.1C251.8,370.5,253.4,357.7,246.3,357.1L246.3,357.1z M286.2,387.9
+    l-20.3,19c0,0,47.7,33.5,47.7,106.7c0,73.1-47.4,104.4-47.4,104.4l19.8,21.2c0,0,57.4-46.4,57.4-125.6
+    C343.4,434.4,286.2,387.9,286.2,387.9L286.2,387.9z"/>
+</g>
+</svg>`;
+const mute = /*`<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" clip-rule="evenodd" />
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2" />
+</svg>`;*/
+
+`<?xml version="1.0" encoding="utf-8"?>
+<!-- Generator: Adobe Illustrator 24.0.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+   viewBox="0 0 393.5 453.8" style="enable-background:new 0 0 393.5 453.8;" xml:space="preserve">
+<style type="text/css">
+  .st0{fill:#FFFFFF;stroke:#CCCCCC;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;}
+  .st1{fill:#FFFFFF;stroke:#CCCCCC;stroke-width:2;stroke-miterlimit:10;}
+</style>
+<g id="svg2" inkscape:export-filename="/home/qubodup/x22.png" inkscape:export-xdpi="90" inkscape:export-ydpi="90" inkscape:version="0.91 r13725" sodipodi:docname="minimal speaker icon.svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:svg="http://www.w3.org/2000/svg">
+  
+    <sodipodi:namedview  bordercolor="#666666" borderopacity="1.0" fit-margin-bottom="0" fit-margin-left="0" fit-margin-right="0" fit-margin-top="0" id="base" inkscape:current-layer="layer1" inkscape:cx="214.07975" inkscape:cy="309.42562" inkscape:document-units="px" inkscape:object-nodes="true" inkscape:pageopacity="0.0" inkscape:pageshadow="2" inkscape:snap-bbox="false" inkscape:snap-object-midpoints="true" inkscape:snap-smooth-nodes="true" inkscape:window-height="865" inkscape:window-maximized="0" inkscape:window-width="1734" inkscape:window-x="73" inkscape:window-y="130" inkscape:zoom="0.70710678" pagecolor="#ffffff" showgrid="false" units="px">
+    </sodipodi:namedview>
+  <g id="layer1" transform="translate(-55,-285.4683)" inkscape:groupmode="layer" inkscape:label="Layer 1">
+    
+      <path id="path4241" inkscape:connector-curvature="0" sodipodi:nodetypes="ccssccccscccccccsscccscccccccccsssccccccscccc" class="st0" d="
+      M345.7,340.2l0,0.2l-19.5,16.8c0,0,12.9,12.2,26.2,38.1c13.3,25.9,26.7,65.3,26.7,119s-13.5,92-26.7,117s-26,36.1-26,36.1l18.1,17
+      c0,0,21.9-19.8,36.1-46.5c14.2-26.8,28.2-67.6,28.2-123.6c0-56-14.1-97.7-28.2-125.3C366.5,361.4,345.7,340.2,345.7,340.2
+      L345.7,340.2z M246.3,357.1c-2.4-0.2-5.7,1.2-10.4,4.4c-18.8,12.9-87.1,87.3-87.1,87.3c-0.6,0-1.1,0.2-1.6,0.3H94.6V580h54.3
+      c0,0,67.1,74.8,87.1,87.6c20,12.9,15.9-7.1,15.9-15.9V377.1C251.8,370.5,253.4,357.7,246.3,357.1L246.3,357.1z M286.2,387.9
+      l-20.3,19c0,0,47.7,33.5,47.7,106.7c0,73.1-47.4,104.4-47.4,104.4l19.8,21.2c0,0,57.4-46.4,57.4-125.6
+      C343.4,434.4,286.2,387.9,286.2,387.9L286.2,387.9z"/>
+    
+      <path id="path2842" inkscape:connector-curvature="0" sodipodi:nodetypes="ccssccccscccccccsscccscccccccccsssccccccscccc" class="st1" d="
+      M345.7,340.2l0,0.2l-19.5,16.8c14,13.2,13.3,12.9,26.2,38.1c13.3,25.9,26.7,65.3,26.7,119s-13.5,92-26.7,117s-26,36.1-26,36.1
+      l18.1,17c0,0,21.9-19.8,36.1-46.5c14.2-26.8,28.2-67.6,28.2-123.6c0-56-14.1-97.7-28.2-125.3C366.5,361.4,345.7,340.2,345.7,340.2
+      L345.7,340.2z M246.3,357.1c-2.4-0.2-5.7,1.2-10.4,4.4c-18.8,12.9-87.1,87.3-87.1,87.3c-0.6,0-1.1,0.2-1.6,0.3H94.6V580h54.3
+      c0,0,67.1,74.8,87.1,87.6c20,12.9,15.9-7.1,15.9-15.9V377.1C251.8,370.5,253.4,357.7,246.3,357.1L246.3,357.1z M286.2,387.9
+      l-20.3,19c0,0,47.7,33.5,47.7,106.7c0,73.1-47.4,104.4-47.4,104.4l19.8,21.2c0,0,57.4-46.4,57.4-125.6
+      C343.4,434.4,286.2,387.9,286.2,387.9L286.2,387.9z"/>
+  </g>
+</g>
+<g id="Layer_2">
+  
+    <rect x="192.6" y="10" transform="matrix(0.6658 -0.7462 0.7462 0.6658 -99.492 229.8913)" class="st1" width="28.4" height="431.9"/>
+</g>
+</svg>`;
+
+const playButton = document.querySelector('.play-button');
+const video = document.getElementById('video');
+const timeline = document.querySelector('.timeline');
+const soundButton = document.querySelector('.sound-button');
+const fullscreenButton = document.querySelector('.fullscreen-button');
+const videoContainer = document.querySelector('.video-player');
+let isFullScreen = false;
+
+const links = [];
+links[0]= "../loader1/index.html";
+links[1]= "../loader2/index.html";
+links[2]= "../loader4/index.html";
+
+function loadNew() {
+
+let random_url = Math.floor(Math.random() * 3); 
+
+ //console.log(random_url);
+  location.href = links[random_url];
+}
+
+playButton.addEventListener('click', function () {
+  if (video.paused) {
+    //video.play();
+    loadNew();
+    videoContainer.classList.add('playing');
+    playButton.innerHTML = pause;
+
+
+  } else {
+    //video.pause();
+    videoContainer.classList.remove('playing');
+    playButton.innerHTML = play;
+    loadNew();
+  }
+})
+
+video.onended = function () {
+  playButton.innerHTML = play;
+}
+
+video.ontimeupdate = function () {
+  const percentagePosition = (100*video.currentTime) / video.duration;
+  timeline.style.backgroundSize = `${percentagePosition}% 100%`;
+  timeline.value = percentagePosition;
+}
+
+timeline.addEventListener('change', function () {
+  const time = (timeline.value * video.duration) / 100;
+  video.currentTime = time;
+});
+
+soundButton.addEventListener('click', function () {
+  video.muted = !video.muted;
+  soundButton.innerHTML = video.muted ? mute : sound;
+  loadNew();
+});
+
+fullscreenButton.addEventListener('click', function () {
+  loadNew();
+  if (!isFullScreen) {
+    if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } else if (video.webkitRequestFullscreen) { /* Safari */
+    video.webkitRequestFullscreen();
+  } else if (video.msRequestFullscreen) { /* IE11 */
+    video.msRequestFullscreen();
+  }
+  } else {
+    if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+  }
+});
